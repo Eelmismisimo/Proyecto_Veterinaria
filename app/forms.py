9d2103ad3b250eb45f77a55addc60ca1ff97691d
Eelmismisimo <agusting.cdr@gmail.com>
2025-10-27 22:02:07 -0300

    Commit 6 models(corregido, de nuevo pero el user),A

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
+from models import UsuarioEx
 
-
+class FormRegistroEx(UserCreationForm):
+    class Meta:
+        model = UsuarioEx
+        fields = ['username', 'email', 'password1', 'password2', 'rut', 'telefono', 'tipo_usuario']
--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-class UsuarioEx(models.Model):
-        user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class UsuarioEx(AbstractUser):
         rut = models.CharField(max_length=10)
         telefono = models.IntegerField()
         tipo_Usuario = models.CharField(max_length=20, choices=[('Administrador','Administrador'),
